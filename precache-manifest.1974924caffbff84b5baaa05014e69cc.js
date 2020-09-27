17567d53db7ad3c8e79c26ce84811ce99fbdda2b
Rian Mandala Putra <rianmandala959@gmail.com>
2020-09-27 22:58:45 +0700

    Updates

---
+++ precache-manifest.1974924caffbff84b5baaa05014e69cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e5dd09f2a34bb5dd11dee7734e604c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce461a2f2c3b44caed6e",
+    "url": "/static/css/main.608245a1.chunk.css"
+  },
+  {
+    "revision": "723616059aae0afe108b",
+    "url": "/static/js/2.6a246e94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a246e94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce461a2f2c3b44caed6e",
+    "url": "/static/js/main.fa6f3f71.chunk.js"
+  },
+  {
+    "revision": "bf250259d9fd61dc7dbe",
+    "url": "/static/js/runtime-main.ecec8c93.js"
+  }
+]);
